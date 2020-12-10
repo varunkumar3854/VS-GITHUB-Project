d911887d1979c4698ffc64669b95a4496d9015f7
varunkumar3854 <varunkumar3854@gmail.com>
2020-12-10 11:31:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS_GITHUB_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
